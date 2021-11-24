f43f0e2a43a13ba23a00854bdb6c0a034897c819
Nyanyan <46193012+zentox@users.noreply.github.com>
2021-11-24 18:02:49 +0100

    Fix invalid code and wrong size of float

--- 20211124/codes/Person_Static.java
+++ 20211124/codes/Person_Static.java
@@ -1,10 +1,10 @@
 public class Person {
-  private String name;
+  private static String name;
 
   public Person(String name) {
     Person.name = name;
   }
-  
+
   public String getName() {
     return name;
   }
